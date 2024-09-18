695ffed13e5194a2526c07b1927be612cacc71cb
Swapnil Sarkar <swapnilmkab@gmail.com>
2024-09-18 21:24:39 +0530

    authentcation completed

---
+++ server/types/mongooseTypes.ts
@@ -0,0 +1,8 @@
+import mongoose from "mongoose";
+
+//constants
+export const mongoString = mongoose.Schema.Types.String;
+
+//types
+export type mongoIdType = mongoose.Schema.Types.ObjectId;
+export type mongoStringType = mongoose.Schema.Types.String;
