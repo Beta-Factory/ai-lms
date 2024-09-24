ba0ae2eca577eb7ec1029aa6b188e2cde395ba8e
Shaikh Parwez Hasim <shaikhparwez963@gmail.com>
2024-09-24 12:22:39 +0530

    pdf issue identified and collection deletion route made

--- server/routes/ai.routes.ts
+++ server/routes/ai.routes.ts
@@ -1,9 +1,11 @@
 import { Router } from "express";
 import { trainPdf } from "../controllers/ai.controller";
 import { AIChat } from "../LangChain/Chat";
+import { deleteCollection } from "../utils/MemoryDeletion";
 
 const router = Router();
 
 router.post("/chat", AIChat);
+router.delete("/mem-wipe", deleteCollection);
 
 export default router;
